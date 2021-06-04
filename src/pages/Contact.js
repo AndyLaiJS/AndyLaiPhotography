439703b609be76c55b856da25903a5fb974af2bf
AndyPhoenix9879 <lai9879m@gmail.com>
2021-06-05 01:19:56 +0800

    Contact.js: content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
+import "../Pages.css";
 
 export default function Contact() {
-	const [count, setCount] = useState(0);
-
-	const increment = () => {
-		setCount(count + 1);
-	};
 	return (
 		<div>
-			<h1> {count} </h1>
-			<button onClick={increment}>INCREMENT</button>
+			<div className="header">I'd love to hear from you!</div>
+
+			<hr></hr>
+			<div
+				className="desc"
+				style={{
+					textAlign: "center",
+					width: "80%",
+					padding: "0 8%",
+					margin: "auto",
+				}}
+			>
+				The Chinese University of Hong Kong | Shatin, New Territories, Hong Kong
+			</div>
+			<div style={{ fontSize: "1.3vh", letterSpacing: "1px", padding: "0.8%" }}>
+				(+852) 69949398 | (+673) 7288098 |{" "}
+				<a href="mailto:andylaijs@gmail.com" style={{ color: "black" }}>
+					andylaijs@gmail.com
+				</a>
+			</div>
+			<hr></hr>
+
+			<div className="fContainer" style={{ paddingBottom: "0" }}>
+				<div className="icons">
+					<a href="https://www.linkedin.com/in/laijianshin/" target="_blank">
+						<img src="/socialicons/linkedin.png" />
+					</a>
+					<a href="https://www.instagram.com/lai9879/" target="_blank">
+						<img src="/socialicons/instagram.png" />
+					</a>
+					<a href="https://github.com/AndyPhoenix9879" target="_blank">
+						<img src="/socialicons/github.png" />
+					</a>
+				</div>
+			</div>
+
+			<img
+				className="picture"
+				src="/photos/Dusk_Hassanil_Bolkiah_Mosque_Lai-01-10-16.jpg"
+				onContextMenu={(e) => e.preventDefault()}
+			/>
 		</div>
 	);
 }
