17293a0c8f7f9931ae18d9d7f2be6e55d251795d
AndyPhoenix9879 <lai9879m@gmail.com>
2021-06-11 22:19:40 +0800

    About: content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function About() {
-	return <h1> About me </h1>;
+	return (
+		<div>
+			<h1>About Me</h1>
+			<p className="desc">
+				My name is Lai Jian Shin; you can call me Andy. I am a student studying
+				at the Chinese University of Hong Kong - and I practice photography as a
+				side hobby and have been very passionate with the art since 2013.{" "}
+				<br></br>
+				<br></br>I had my first camera in 2015, the Sony A6000, and have
+				shooting with it together with the kit lens (the 16-50mm and 55-210mm)
+				that came with it ever since. It's not the most impressive set of gears
+				but it gets the work done!{" "}
+			</p>
+
+			<p className="desc">
+				Thank you for taking the time in visiting and viewing my work and
+				galleries!
+			</p>
+		</div>
+	);
 }
