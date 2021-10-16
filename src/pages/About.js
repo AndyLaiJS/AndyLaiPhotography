d699f69f2575e58171a388c9ca8699e0c2c8c7d9
AndyPhoenix9879 <lai9879m@gmail.com>
2021-10-16 16:13:01 +0800

    About: Updated content

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,26 +10,29 @@ export default function About() {
 				<br></br>
 				Firstly, thank you for visiting my photography website. My name is Lai
 				Jian Shin; you can call me Andy! I am a Computer Science student at the
-				Chinese University of Hong Kong. I have built this website not only to
-				display my portfolio and my work in photography but also to exercise my
-				web development. I hoped to create and design a web page design that can
-				reflect my own work in photography as well - a look and feel of
+				Chinese University of Hong Kong. I built this website obviously to
+				showcase my work and establish it as my portfolio for my photography,
+				but I also used this project of building this website from scratch as a
+				means to exercise my web development (cause you know, my future kind of
+				depends on it). In designing this website, I hoped that its design is a
+				reflection of my work in photography as well - a look and feel of
 				serenity, elegance and youthfulness.
 				<br></br>
-				<br></br>I have been practicing photography as a side hobby since 2013.
-				After a trip to Turkey, my mom and I realized that it’d be better if we
-				had our hands with an actual camera - not a 5 megapixel smartphone
-				(although that’s really where I started off in my photography
-				adventure). That camera we later got was the Sony A6000, and it has been
-				amazingly faithful to me ever since. I have and am still shooting with
-				it together with the kit lens that came with it - the 16-50mm and
-				55-210mm kit lenses. Not the best glass out there, but it gets the work
-				done well enough!
+				<br></br>I have been practicing photography as a hobby since 2013. After
+				a trip to Turkey, my mom and I realized that it’d be better if we had
+				our hands with an actual camera - not a 5 megapixel smartphone (although
+				that’s really where I started off in my journey into photography). That
+				camera we later got was the Sony A6000, and it has been amazingly
+				faithful to me ever since. I have and am still shooting with it together
+				with the kit lens that came with it - the 16-50mm and 55-210mm kit
+				lenses. Not the best glass out there, but it gets the work done.
 				<br></br>
 				<br></br>I hope my work and my galleries are inspiring and enjoyable.
-				Thank you, truly, for having taken the time to read through all these
-				and to view my work and my galleries. If you want to check out the code
-				base, feel free to check out my github!
+				Thank you, really, for having taken the time to read through all these
+				and to view my work and my galleries. Feel free to follow me at my
+				instagram and contact me about anything at all! Happy to connect! For
+				the software people that stumbled here somehow, I have my code base in
+				my github that you are more than free to check em' out!
 				<br></br>
 				<br></br>
 				Best,
